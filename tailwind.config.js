cf59fca2f282dd3943a1f41aa75490424d4ed941
albidnawar <albidnawar@gmail.com>
2024-04-29 15:12:40 +0600

    added landing page nav bar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    //...
+    daisyui: {
+      themes: ["light", "dark", "cupcake"],
+    },
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  }
+
+  
